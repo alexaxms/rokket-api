7326735a323843df4541d2fcd2649e2740096875
Rodrigo Naranjo <rodnaranjop@gmail.com>
2020-07-10 03:51:03 -0400

    [:zap:] Added main server file

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+import books from "./routes/books.routes";
+import { API_PORT } from "./config/app.config";
+import startDatabaseConnection from "./mongo/connection";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("[:status] :method\t:url"));
+
+app.use("/api/books", books);
+
+startDatabaseConnection()
+  .then(() => {
+    app.listen(API_PORT, () => {
+      console.log(`[:${API_PORT}] Server running...`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Server failed to start due to error ${err}`);
+  });
